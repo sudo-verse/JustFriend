fb25cc0862af25535423e930d631c11442920d7f
Devashish gupta <ashishkrgupta.hajipur@gmail.com>
2026-02-10 19:23:34 +0530

    fix bug

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:7776";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7776" : "/api";
 export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,11 @@ import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 const createSocketConnection = () => {
-    return io(BASE_URL);
+    if (location.hostname === "localhost") {
+        return io(BASE_URL);
+    } else {
+        return io("/", { path: "/api/socket.io" });
+    }
 }
 
 export default createSocketConnection;
